6faf9c6157acd93c45363b8c3b8d08eb3c43f674
GavrilinYP <Gavrilin-Yuriy@mail.ru>
2020-06-25 22:49:48 +0300

    Добавление функций
    Добавление функций help и numbers

--- bot.py
+++ bot.py
@@ -2,23 +2,37 @@ i = 0
 b = []
 
 
-def main(bot):  # сюда писать только функции которые трубуют декоратора
+
+def main(bot):
     @bot.message_handler(commands=['start', 'go'])
     def start_handler(message):
-        bot.send_message(message.chat.id, 'заебись')
+        bot.send_message(message.chat.id, 'Вас приветсвует Emergency Notification bot! \n'
+                                          'Введите /help для получения справки по командам')
+
+    @bot.message_handler(commands=['help', 'h'])
+    def help_handler(message):
+        bot.send_message(message.chat.id,  'Список команд:\n1) /help - получение справки'
+                                          '\n2) /numbers - номера телефонов спецслужб'
+                                          '\n3) /reg - регистрация в приложении'
+                                           '\n4) /edit - смена адресса регистрации')
+
+    @bot.message_handler(commands=['numbers', 'h'])
+    def numbers_handler(message):
+        bot.send_message(message.chat.id, '101 - пожарная служба'
+                                          '\n102 - полиция'
+                                          '\n103 - скорая помощь'
+                                          '\n104 - газовая служба'
+                                          '\n112 - мобильный агрегатор МЧС')
+
 
     @bot.message_handler(content_types=['text'])
     def prp(massage):
         global i, b
+        print(massage.from_user.first_name, '[',massage.from_user.id,']:', massage.text)
         i += 1
-        print(i)
+        print('Сигнал',i,'/10')
         b.append(massage.chat.id)
-        if i == 3:
+        print(b)
+        if i == 2:
             i = 0
-            bot.send_message(b[-1], 'Сон')
-
-
-def mailing(bot, text):
-    v = [1011917065, 809971387]
-    for val in v:
-        bot.send_message(val, text)
+            bot.send_message(b[-1], 'Вы находитесь в опасности')
