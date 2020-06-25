5b8344598b0b06c5a5d84a653716d43258464864
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-06-25 18:14:32 +0300

    тестовый

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
-import telebot
-
-bot = telebot.TeleBot('1213132925:AAE2jwkv00Xgl6AeyQj9UxgkZE5QoHpm2fU')
-
-
-@bot.message_handler(commands=['start', 'go'])
-def start_handler(message):
-    bot.send_message(message.chat.id, 'заебись')
-    bot.register_next_step_handler(message, prp)
+i = 0
+b = []
 
 
-def prp(massage):
-    bot.send_message(massage.chat.id, 'Вы сказали: {}'.format(massage.text))
+def main(bot):
+    @bot.message_handler(commands=['start', 'go'])
+    def start_handler(message):
+        bot.send_message(message.chat.id, 'заебись')
 
-
-bot.polling(none_stop=True, interval=0)
+    @bot.message_handler(content_types=['text'])
+    def prp(massage):
+        global i, b
+        i += 1
+        print(i)
+        b.append(massage.chat.id)
+        if i == 3:
+            i = 0
+            bot.send_message(b[-1], 'Сон')
---
+++ test.py
@@ -0,0 +1,35 @@
+from threading import Thread
+from telebot import TeleBot
+import bot as t
+
+
+def main(bot):
+    variable = Thread(target=start_bot, args=(bot,))
+    variable1 = Thread(target=t.main, args=(bot,))
+    variable.start()
+    variable1.start()
+    while True:
+        a = input(': ')
+        if a.lower() == 'пожар':
+            mailing(bot, a)
+        elif a.lower() == 'стоп':
+            variable.join()
+            variable1.join()
+            bot.stop_polling()
+        else:
+            print(a)
+
+
+def start_bot(bot):
+    bot.polling(none_stop=True, interval=0)
+
+
+def mailing(bot, text):
+    v = [1011917065, 809971387]
+    for val in v:
+        bot.send_message(val, text)
+
+
+if __name__ == '__main__':
+    bot = TeleBot('1213132925:AAE2jwkv00Xgl6AeyQj9UxgkZE5QoHpm2fU')
+    main(bot)
