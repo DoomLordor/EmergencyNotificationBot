9963b093c5252fdcd58c9ef6e50628c3d8845704
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-06-25 15:19:19 +0300

    Добовление функции вывода сообщения

--- bot.py
+++ bot.py
@@ -6,6 +6,11 @@ bot = telebot.TeleBot('1213132925:AAE2jwkv00Xgl6AeyQj9UxgkZE5QoHpm2fU')
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
     bot.send_message(message.chat.id, 'заебись')
+    bot.register_next_step_handler(message, prp)
+
+
+def prp(massage):
+    bot.send_message(massage.chat.id, 'Вы сказали: {}'.format(massage.text))
 
 
 bot.polling(none_stop=True, interval=0)
