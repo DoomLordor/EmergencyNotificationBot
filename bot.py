962d741471430e24a4011b915ff17044d6a40816
Vladislav <Bitsbobbom@gmail.com>
2020-06-26 14:26:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,4 +13,21 @@ def prp(massage):
     bot.send_message(massage.chat.id, 'Вы сказали: {}'.format(massage.text))
 
 
+<<<<<<< Updated upstream
 bot.polling(none_stop=True, interval=0)
+=======
+    @bot.message_handler(content_types=['text'])
+    def prp(massage):
+        global i, b
+        print(massage.from_user.first_name, '[',massage.from_user.id,']:', massage.text)
+        response_keys = key_phrase_extraction_example(client, massage.text)
+        bot.send_message(massage.chat.id, response_keys)
+        i += 1
+        print('Сигнал',i,'/10')
+        b.append(massage.chat.id)
+        print(b)
+        if i == 2:
+            i = 0
+            bot.send_message(b[-1], 'Вы находитесь в опасности')
+
+>>>>>>> Stashed changes
