4e1b9ce9365989d0af396a9a5fcf2879afab849a
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-06-27 12:09:29 +0300

    Добовление работы с базой данных

--- bot.py
+++ bot.py
@@ -22,3 +22,27 @@ def mailing(bot, text):
     v = [1011917065, 809971387]
     for val in v:
         bot.send_message(val, text)
+
+
+def new_user(connect, user_id):
+    cursor = connect.cursor()
+    cursor.execute("INSERT INTO client(telegram_id, home_place, stud_place, work_place) VALUES (%s, %s, %s, %s)",
+                   (user_id, '', '', ''))
+    connect.commit()
+    cursor.close()
+
+
+def set_address(connect, user_id, type_address, address):
+    cursor = connect.cursor()
+    cursor.execute("UPDATE client SET %s_place = %s WHERE telegram_id==%s", (type_address, address, user_id))
+    connect.commit()
+    cursor.close()
+
+
+def get_user(connect, address):
+    cursor = connect.cursor()
+    cursor.execute("SELECT telegram_id FROM client WHERE home_place=%s or stud_place=%s or work_place=%s",
+                   (address, address, address))
+    rows = cursor.fetchall()
+    cursor.close()
+    return rows
