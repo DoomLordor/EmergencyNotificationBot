059064e6e82ba2f880dab9294647babf6f02c49b
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-06-27 17:16:05 +0300

    добовление конфигурационного файла

---
+++ model/config.py
@@ -0,0 +1,7 @@
+token_telegram_bot = '1213132925:AAE2jwkv00Xgl6AeyQj9UxgkZE5QoHpm2fU'
+database_connect = {'dbname': 'postgres',
+                    'user': 'DoomLord@client-storages',
+                    'host': 'client-storages.postgres.database.azure.com',
+                    'password': 'PASH51379!',
+                    'port': '5432',
+                    'sslmode': 'require'}
