aa2bff75f4109b52df9397dcade94aa47c6d3549
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-06-27 17:16:38 +0300

    Редактирование импорта и добавление работы с базой данных

--- test.py
+++ test.py
@@ -1,34 +1,36 @@
-from threading import Thread
 from telebot import TeleBot
-import bot as t
+from psycopg2 import connect
+from threading import Thread
+from model.database import get_all_user
+from TelegramBot.bot_logic import handler, mailing
+from model.config import token_telegram_bot, database_connect
 
 
-def main(bot):
-    with open('Base.txt', 'r') as f:
-        text = f.read().split('\n')
-        text.pop()
-        t.v = list(map(int, text))
+def main(bot, conn):
     variable = Thread(target=start_bot, args=(bot,))
-    variable1 = Thread(target=t.main, args=(bot,))
+    variable1 = Thread(target=handler, args=(bot, conn))
     variable.start()
     variable1.start()
     while True:
         a = input('Сообщение: ')
         if a.lower() == 'пожар':
-            t.mailing(bot, a)
+            mailing(bot, a, get_all_user(conn))
         elif a.lower() == 'стоп':
             variable.join()
             variable1.join()
             bot.stop_polling()
         else:
-            t.mailing(bot, a)
+            mailing(bot, a, connect)
             print(a)
 
 
 def start_bot(bot):
-    bot.polling(none_stop=True, interval=0)
+    bot.polling(none_stop=True, interval=1)
 
 
 if __name__ == '__main__':
-    bot = TeleBot('1213132925:AAE2jwkv00Xgl6AeyQj9UxgkZE5QoHpm2fU')
-    main(bot)
+    bot = TeleBot(token_telegram_bot)
+    print('Бот подключен')
+    conn = connect(**database_connect)
+    print('База данных подключена')
+    main(bot, conn)
