0d7c3a9299924fb874520010ab3fdf1374feb862
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-06-28 16:42:34 +0300

    упращение стартового файла

--- test.py
+++ test.py
@@ -1,34 +1,33 @@
-import http.client
 from telebot import TeleBot
 from psycopg2 import connect
 from threading import Thread
-from model.database import get_all_user, get_user
-from TelegramBot.bot_logic import handler, mailing, emergency_mailing, danger_user
+from model.logic import dangerous_start, dangerous_stop, timer, get_ip, danger, flag
+from model.database import get_all_user
+from TelegramBot.bot_logic import handler, mailing
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 from model.config import token_telegram_bot, database_connect, key, endpoint, Disasters
 
 
 def main(bot, conn, client):
-    variable = Thread(target=start_bot, args=(bot,))
-    variable1 = Thread(target=handler, args=(bot, conn, client))
-    variable.start()
-    variable1.start()
+    bot_work = Thread(target=start_bot, args=(bot,))
+    bot_logic = Thread(target=handler, args=(bot, conn, client))
+    timer_danger = Thread(target=timer, args=(danger, flag))
+    bot_work.start()
+    bot_logic.start()
+    timer_danger.start()
     ip = get_ip()
+
     mailing(bot, f'Бот запущен на сервере у {ip}', [1011917065, 809971387, 453207183, 283130759])
+
     while True:
         message = input('Сообщение: ')
         if message.lower() in Disasters:
-            street = input('Введите улицу проишествия: ').lower()
-            num = input('Введите дом проишествия: ')
-            users = get_user(conn, f'{street}:{num}')
-            message = f'По аддресу {street.title()} {num} произошло проишествие: {message}'
-            emergency_mailing(bot, message, users)
-            danger_user(users, f'{street.title()} {num}')
+            dangerous_start(message)
+        elif message.lower() == 'проишествие стоп':
+            dangerous_stop()
         elif message.lower() == 'стоп':
-            bot.stop_polling()
-            variable.join()
-            variable1.join()
+            bot_stop(bot, bot_work, bot_logic, timer_danger)
             mailing(bot, 'Бот остановлен', [1011917065, 809971387, 453207183, 283130759])
             break
         else:
@@ -40,16 +39,17 @@ def start_bot(bot):
     bot.polling(none_stop=True, interval=1)
 
 
+def bot_stop(bot, *flows):
+    bot.stop_polling()
+    flag[0] = False
+    for flow in flows:
+        flow.join()
+
+
 def authenticate_client():
     ta_credential = AzureKeyCredential(key)
     text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=ta_credential)
     return text_analytics_client
-
-
-def get_ip():
-    conn = http.client.HTTPConnection("ifconfig.me")
-    conn.request("GET", "/ip")
-    return conn.getresponse().read().decode("utf-8")
 
 
 if __name__ == '__main__':
